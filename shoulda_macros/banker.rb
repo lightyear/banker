54fbe42f4678ffd31aefcee6386318d6fb6da5d1
Steve Madsen <github@lightyearsoftware.com>
2009-10-07 20:35:54 -0400

    should_not forms of the existing Shoulda macros.

--- shoulda_macros/banker.rb
+++ shoulda_macros/banker.rb
@@ -7,10 +7,24 @@ module Banker
       end
     end
   
+    def should_not_cache_page(description, &block)
+      should "not cache #{description} page" do
+        cache_path = instance_eval(&block)
+        assert_page_not_cached cache_path
+      end
+    end
+  
     def should_expire_page(description, &block)
       should "expire #{description} page" do
         cache_path = instance_eval(&block)
         assert_page_expired cache_path
+      end
+    end
+    
+    def should_not_expire_page(description, &block)
+      should "not expire #{description} page" do
+        cache_path = instance_eval(&block)
+        assert_page_not_expired cache_path
       end
     end
     
@@ -20,9 +34,21 @@ module Banker
       end
     end
     
-    def should_expire_action(description, action)
+    def should_not_cache_action(description, action)
+      should "not cache #{description} action" do
+        assert_action_not_cached(action)
+      end
+    end
+    
+    def should_expire_action(description, key)
       should "expire #{description} action" do
         assert_action_expired(action)
+      end
+    end
+    
+    def should_not_expire_action(description, key)
+      should "not expire #{description} action" do
+        assert_action_not_expired(action)
       end
     end
     
@@ -32,9 +58,21 @@ module Banker
       end
     end
     
+    def should_not_cache_fragment(description, key)
+      should "not cache #{description}" do
+        assert_fragment_not_cached(key)
+      end
+    end
+    
     def should_expire_fragment(description, key)
       should "expire #{description}" do
         assert_fragment_expired(key)
+      end
+    end
+    
+    def should_not_expire_fragment(description, key)
+      should "not expire #{description}" do
+        assert_fragment_not_expired(key)
       end
     end
   end
